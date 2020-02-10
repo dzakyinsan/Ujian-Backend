3504b734c52435a82eb87bb38d6bd37378780a82
dzakyinsan <dzakyinsan20@gmail.com>
2020-02-10 15:41:07 +0700

    first commit

--- routers/movcatRouters.js
+++ routers/movcatRouters.js
@@ -4,7 +4,7 @@ const {movcatController}= require('../controller')
 const router=express.Router()
 
 router.get('/get',movcatController.getUsers)
-// router.post('/post',movcatController.postUsers)
+router.post('/post',movcatController.postUsers)
 router.delete('/delete/:id',movcatController.deleteUsers)
 
 module.exports=router
